dd7eda64155a507b07d209aa64fb93c7cd1563bd
Priscilla-22 <priscillamw123@gmail.com>
2024-01-15 19:36:51 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Your code here
+class Polygon {
+  constructor(ArrayOfSides) {
+    this.ArrayOfSides = ArrayOfSides;
+  }
+  get countSides() {
+    return `This are the number of sides: ${this.ArrayOfSides}`;
+  }
+  get Perimeter() {
+    return `The perimeter is${this.ArrayOfSides.reduce(
+      (sum, side) => sum + side,
+      0
+    )}`;
+  }
+}
+
+class Triangle extends Polygon {
+  constructor(...ArrayOfSides) {
+    super(ArrayOfSides);
+  }
+  get isValid() {
+    const [a, b, c] = this.ArrayOfSides;
+    return a + b > c && b + c > a && c + a > b;
+  }
+}
+const validTriangle = new Triangle(4, 4, 4);
+
+class Square extends Polygon {
+  constructor(...ArrayOfSides) {
+    super(ArrayOfSides);
+  }
+  get isValid() {
+    const [a, b, c, d] = this.ArrayOfSides;
+    return a === b && b === c && c === d;
+  }
+  get area() {
+    const OneSideLength = this.ArrayOfSides[0];
+    return OneSideLength * OneSideLength;
+  }
+}
+const validSquare = new Square(4, 4, 4, 4);
